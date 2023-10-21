096ec0d6b9e41ee8ed3b037db8838ce089682e56
Leandro Paz <leandropaz@gmail.com>
2023-10-21 18:29:49 -0300

    graphql classes

---
+++ comm_systems/graphql/graph/resolver.go
@@ -0,0 +1,12 @@
+package graph
+
+import "github.com/leandropaz/fullcycle_3.0/comm_systems/graphql/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+	CourseDB   *database.Course
+}
